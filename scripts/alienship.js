5a56d3a776a687711233217d31b802cd4a247a93
Daniel Clements <daniel@daniel-Latitude-E5430-non-vPro.(none)>
2014-04-24 14:09:23 -0400

    added rough skeleton of some functions to be completed later

--- scripts/alienship.js
+++ scripts/alienship.js
@@ -2,4 +2,28 @@ alienship.prototype = new SpaceObject();
 function alienship(canvas){
 	this.canvas = canvas;
 
+	var turnDelay = (new Date()).getTime();
+
+	this.getTurnDelay = function(){
+		return turnDelay;
+	}
+
+	function init(){
+
+	}	
+
+	this.getBoundingBox = function getBoundingBox(){
+
+	}
+
+
+
+	this.paint = function(){
+
+	}
+
+	this.init = init;
+
+	this.init();
+	return this;
 }
