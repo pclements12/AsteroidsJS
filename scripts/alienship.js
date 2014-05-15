959a8dbb0a6a8302e656ed76e25c9acd0712df24
Daniel Clements <daniel@daniel-Latitude-E5430-non-vPro.(none)>
2014-05-14 23:18:31 -0400

    fixed typos

--- scripts/alienship.js
+++ scripts/alienship.js
@@ -2,10 +2,17 @@ alienship.prototype = new SpaceObject();
 function alienship(canvas){
 	this.canvas = canvas;
 
+<<<<<<< HEAD
 	var turnDelay = (new Date()).getTime();
+=======
+	this.angle = Math.PI / 2; //constant, straight up
 
-	this.getTurnDelay = function(){
-		return turnDelay;
+	var lastTurn = (new Date()).getTime();
+	var shotDelay = 0; //milliseconds
+>>>>>>> fixed typos
+
+	this.getLastTurn = function(){
+		return lastTurn;
 	}
 
 	function init(){
